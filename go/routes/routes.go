89460f1252792686e6401b05e6c48fe5f420b84e
jamessouth <jamesdanielsouth@gmail.com>
2025-03-02 19:09:37 -0600

    change slash route

--- go/routes/routes.go
+++ go/routes/routes.go
@@ -1,16 +1,14 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/HSU-Senior-Project-2025/Cowboy_Cards/go/controllers"
 	"github.com/go-chi/chi/v5"
 )
 
 func Routes(r *chi.Mux, cfg *controllers.Config) {
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world\n"))
-	})
+	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("hello world\n"))
+	// })
 	r.Get("/classes", cfg.GetClasses)
 	r.Get("/flashcard_sets", cfg.GetUsersFlashCardSets)
 	r.Post("/flashcard", cfg.CreateFlashCard)
