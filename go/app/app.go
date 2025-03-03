6356c28d6598fb06ae516ffb0c7f879a75d99cbc
jamessouth <jamesdanielsouth@gmail.com>
2025-03-02 20:38:12 -0600

    change slash route x6

--- go/app/app.go
+++ go/app/app.go
@@ -8,8 +8,8 @@ import (
 	"time"
 
 	"github.com/HSU-Senior-Project-2025/Cowboy_Cards/go/controllers"
-	"github.com/HSU-Senior-Project-2025/Cowboy_Cards/go/routes"
-	"github.com/go-chi/chi/v5"
+	// "github.com/HSU-Senior-Project-2025/Cowboy_Cards/go/routes"
+	// "github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/urfave/negroni/v3"
@@ -44,21 +44,21 @@ func setCacheControlHeader(w http.ResponseWriter, r *http.Request, next http.Han
 }
 
 func Init() {
-	cfg, err := LoadConfig()
-	if err != nil {
-		log.Fatalf("error getting config: %v", err)
-	}
+	// cfg, err := LoadConfig()
+	// if err != nil {
+	// 	log.Fatalf("error getting config: %v", err)
+	// }
 
 	n := negroni.New()
-	n.Use(negroni.NewLogger())
-	n.Use(negroni.NewRecovery())
-	n.Use(negroni.HandlerFunc(setCacheControlHeader))
+	// n.Use(negroni.NewLogger())
+	// n.Use(negroni.NewRecovery())
+	// n.Use(negroni.HandlerFunc(setCacheControlHeader))
 	n.Use(negroni.NewStatic(http.Dir("./dist")))
 
-	r := chi.NewRouter()
-	routes.Routes(r, cfg)
+	// r := chi.NewRouter()
+	// routes.Routes(r, cfg)
 
-	n.UseHandler(r)
+	// n.UseHandler(r)
 
 	port, ok := os.LookupEnv("PORT")
 	if !ok {
