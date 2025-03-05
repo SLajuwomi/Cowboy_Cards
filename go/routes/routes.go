4e45f17392457de5df1623134fdbf79a5ecda03e
King Sharif <ao.sharifahmed@gmail.com>
2025-03-04 19:23:11 -0600

    i get 200 on server but not frontend

--- go/routes/routes.go
+++ go/routes/routes.go
@@ -3,15 +3,16 @@ package routes
 import (
 	"github.com/HSU-Senior-Project-2025/Cowboy_Cards/go/controllers"
 	"github.com/go-chi/chi/v5"
-
+	"github.com/go-chi/cors"
 )
 
 func Routes(r *chi.Mux, cfg *controllers.Config) {
 
-
 	// Auth routes
-	r.Post("/signup", cfg.Signup)
-	r.Post("/login", cfg.Login)
+	r.Route("/api/auth", func(r chi.Router) {
+		r.Post("/signup", cfg.Signup)
+		r.Post("/login", cfg.Login)
+	})
 
 	// Protected routes
 	r.Group(func(r chi.Router) {
@@ -19,16 +20,14 @@ func Routes(r *chi.Mux, cfg *controllers.Config) {
 		
 		r.Get("/classes", cfg.GetClasses)
 	
-	// r.Get("/flashcard_sets", cfg.GetUsersFlashCardSets)
 		r.Get("/flashcard_set", cfg.GetFlashCardSet)
-	r.Post("/flashcard_set", cfg.CreateFlashCardSet)
-	r.Put("/flashcard_set", cfg.UpdateFlashCardSet)
-	r.Delete("/flashcard_set", cfg.DeleteFlashCardSet)
+		r.Post("/flashcard_set", cfg.CreateFlashCardSet)
+		r.Put("/flashcard_set", cfg.UpdateFlashCardSet)
+		r.Delete("/flashcard_set", cfg.DeleteFlashCardSet)
 
-	r.Get("/flashcard", cfg.GetFlashCard)
-	r.Post("/flashcard", cfg.CreateFlashCard)
-			r.Put("/flashcard", cfg.UpdateFlashCard)
+		r.Get("/flashcard", cfg.GetFlashCard)
+		r.Post("/flashcard", cfg.CreateFlashCard)
+		r.Put("/flashcard", cfg.UpdateFlashCard)
 		r.Delete("/flashcard", cfg.DeleteFlashCard)
-
 	})
 }
