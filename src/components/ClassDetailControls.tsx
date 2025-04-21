01ebda591132b390abea15d027d715399986dacd
SLajuwomi <stephenlajuwomi@gmail.com>
2025-04-21 18:28:04 -0500

    fixed color of back button and alignment issue

--- src/components/ClassDetailControls.tsx
+++ src/components/ClassDetailControls.tsx
@@ -1,37 +1,35 @@
-import { IonButton, IonIcon, IonCardContent } from '@ionic/react';
-import { arrowBackOutline, createOutline, addOutline } from 'ionicons/icons';
+import { IonButton, IonIcon } from '@ionic/react';
+import { addOutline, arrowBackOutline, createOutline } from 'ionicons/icons';
 
 const ClassDetailControls = (props) => {
   return (
-    <IonCardContent>
-      <div className="flex justify-between items-center mb-4">
-        <IonButton routerLink="/home" color="medium">
-          <IonIcon slot="start" icon={arrowBackOutline} />
-          Back
-        </IonButton>
-        {props.isTeacher && (
-          <div className="flex gap-2">
-            <IonButton
-              onClick={props.onAddSetClick}
-              color="primary"
-              disabled={!props.classId}
-            >
-              <IonIcon slot="start" icon={addOutline} />
-              Add Existing Set
-            </IonButton>
+    <div className="flex justify-between items-center mb-4">
+      <IonButton routerLink="/home" color="primary">
+        <IonIcon slot="start" icon={arrowBackOutline} />
+        Back
+      </IonButton>
+      {props.isTeacher && (
+        <div className="flex gap-2">
+          <IonButton
+            onClick={props.onAddSetClick}
+            color="primary"
+            disabled={!props.classId}
+          >
+            <IonIcon slot="start" icon={addOutline} />
+            Add Existing Set
+          </IonButton>
 
-            <IonButton
-              routerLink={`/create-set?classId=${props.classId}`}
-              color="primary"
-              disabled={!props.classId}
-            >
-              <IonIcon slot="start" icon={createOutline} />
-              Create New Set
-            </IonButton>
-          </div>
-        )}
-      </div>
-    </IonCardContent>
+          <IonButton
+            routerLink={`/create-set?classId=${props.classId}`}
+            color="primary"
+            disabled={!props.classId}
+          >
+            <IonIcon slot="start" icon={createOutline} />
+            Create New Set
+          </IonButton>
+        </div>
+      )}
+    </div>
   );
 };
 
