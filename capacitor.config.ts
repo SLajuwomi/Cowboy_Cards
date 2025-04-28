bf3d5f495f707f53e2ae307d8cdb0424a6f93c18
Patrick Taylor <pwt2104@hsutx.edu>
2025-04-28 17:54:03 -0500

    fix ip for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'Cowboy Cards',
   webDir: 'public',
   server: {
-    url: 'http://10.84.16.22:8080/',// this is the IP from the second line ("Network") of the output of 'npm run dev' that runs the Vite front-end dev server
+    url: 'http://10.84.16.32:8080/',// this is the IP from the second line ("Network") of the output of 'npm run dev' that runs the Vite front-end dev server
     cleartext: true,
   }
 };
