b06f7a72efb8ee95c1382588b4d2f5ee0c20e401
Brock Scoggin <bas2112@hsutx.edu>
2025-04-30 16:09:51 -0500

    fixed blame

--- src/components/ClassDetailControls.tsx
+++ src/components/ClassDetailControls.tsx
@@ -1,50 +1,42 @@
-import { IonButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
-import { addOutline, arrowBackOutline, createOutline, exitOutline } from 'ionicons/icons';
+import { IonButton, IonIcon, IonGrid, IonCol, IonRow } from '@ionic/react';
+import { addOutline, arrowBackOutline, createOutline } from 'ionicons/icons';
 
 const ClassDetailControls = (props) => {
   return (
-    <IonGrid className="mb-4">
+     <IonGrid className="mb-4">
       <IonRow className="ion-align-items-center ion-justify-content-between">
-        <IonCol size="12" size-md="auto">
-          <IonButton routerLink="/home" color="primary">
-            <IonIcon slot="start" icon={arrowBackOutline} />
-            Back
-          </IonButton>
-        </IonCol>
+        <IonCol size="12" sizeMd="auto">
+      <IonButton routerLink="/home" color="primary">
+        <IonIcon slot="start" icon={arrowBackOutline} />
+        Back
+      </IonButton>
+      </IonCol>
 
-        <IonCol size="12" size-md="auto" className="flex gap-2 flex-wrap">
-          {props.isTeacher && (
-            <>
-              <IonButton
-                onClick={props.onAddSetClick}
-                color="primary"
-                disabled={!props.classId}
-              >
-                <IonIcon slot="start" icon={addOutline} />
-                Add Existing Set
-              </IonButton>
-
-              <IonButton
-                routerLink={`/create-set?classId=${props.classId}`}
-                color="primary"
-                disabled={!props.classId}
-              >
-                <IonIcon slot="start" icon={createOutline} />
-                Create New Set
-              </IonButton>
-            </>
-          )}
-
-          {/* <IonButton
-             // Add handler here
-            color="danger"
+      {props.isTeacher && (
+        <IonCol
+        size="12"
+        sizeMd="auto"
+        className="flex gap-2 flex-wrap justify-end"
+      >
+          <IonButton
+            onClick={props.onAddSetClick}
+            color="primary"
             disabled={!props.classId}
-            onClick={() => props.onDeleteStudent(props)}
           >
-            <IonIcon slot="start" icon={exitOutline} />
-            Leave Class
-          </IonButton> */}
-        </IonCol>
+            <IonIcon slot="start" icon={addOutline} />
+            Add Existing Set
+          </IonButton>
+
+          <IonButton
+            routerLink={`/create-set?classId=${props.classId}`}
+            color="primary"
+            disabled={!props.classId}
+          >
+            <IonIcon slot="start" icon={createOutline} />
+            Create New Set
+          </IonButton>
+          </IonCol>
+        )}
       </IonRow>
     </IonGrid>
   );
